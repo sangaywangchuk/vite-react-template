642c11fcd7367a875fb8a058a46d21c5d6e9de30
SANGAY WANGCHUK <sangay@SANGAYs-MacBook-Air.local>
2025-02-26 00:33:30 +0600

    first commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,6 @@ export const Route = createFileRoute('/')({
 
 function RouteComponent() {
   return <div className=''>
-    <Button>Click me</Button>
+    <Button>Click m asdfasdf</Button>
   </div>
 }
