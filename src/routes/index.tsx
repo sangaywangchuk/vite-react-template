855886165a49972b8049f4fd75a2b66dbb3a556b
SANGAY WANGCHUK <sangay@SANGAYs-MacBook-Air.local>
2025-02-26 17:11:19 +0600

    asda

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,8 @@ export const Route = createFileRoute('/')({
 });
 
 function RouteComponent() {
+  console.log('asdfasdf');
+
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
       <div className="grid auto-rows-min gap-4 md:grid-cols-3">
