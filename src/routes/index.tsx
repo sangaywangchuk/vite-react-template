4c4bd830e4fb7ead5896ec259c96e8962ed05d83
SANGAY WANGCHUK <sangay@SANGAYs-MacBook-Air.local>
2025-02-26 14:53:42 +0600

    sdfa

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,6 @@ export const Route = createFileRoute('/')({
 
 function RouteComponent() {
   return <div>
-    <Button>Click m asdfasdf asdfas asdfasdfdf</Button>
+    <Button>Click m asdfasdf</Button>
   </div>
 }
