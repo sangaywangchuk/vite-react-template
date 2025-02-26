b2a6a67951d933da12d59b21d4caa05515f22e1a
SANGAY WANGCHUK <sangay@SANGAYs-MacBook-Air.local>
2025-02-26 14:30:54 +0600

    msg

--- lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-
-export default {
-    '*/**/*.{ts,tsx}': (stagedFiles) => [
-      'yarn tsc --noEmit',
-    //   `yarn eslint ${stagedFiles.join(' --file ')}`,
-    //   `prettier --write ${stagedFiles.join(' ')} --ignore-unknown`
-    ]
-  };
